18a42594493030f516420f2c318577a739579074
Ravikant <ravidreamer7@gmail.com>
2024-09-12 22:23:05 +0530

    Last bug fix

--- server.js
+++ server.js
@@ -33,9 +33,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("home route");
-});
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
